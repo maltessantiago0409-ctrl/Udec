527469a379bd1488f5cb429fe62642e7fb33c062
maltessantiago0409-ctrl <maltessantiago0409@gmail.com>
2026-03-23 21:32:41 -0500

    imagen

--- git/pericoepstein.py
+++ git/pericoepstein.py
@@ -3,6 +3,8 @@ from tkinter import *
 
 root = tk.Tk()
 root.geometry("350x250")
+root.title("el codigo mas uribista")
+root.iconbitmap(r'C:\Users\Edwin Santiago\OneDrive\Documentos\programacion 2\pdf\IMG\perico.ico')
 
 
 root.mainloop()

624298c06a560a0b434fa77c550a726a45398ed1
maltessantiago0409-ctrl <maltessantiago0409@gmail.com>
2026-03-24 07:52:10 -0500

    actualizar

--- git/pericoepstein.py
+++ git/pericoepstein.py
@@ -4,7 +4,7 @@ from tkinter import *
 root = tk.Tk()
 root.geometry("350x250")
 root.title("Este es el codigo mas uribista de colombia")
-root.iconbitmap(r'C:\Users\Edwin Santiago\OneDrive\Documentos\programacion 2\pdf\IMG\perico.ico')
+root.iconbitmap(r'pdf\IMG\perico.ico')
 
 
 print("hola muy uribista")
